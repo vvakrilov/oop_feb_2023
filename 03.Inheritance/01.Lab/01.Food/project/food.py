519d56c401c52387406ba1f4bfb4b0c2fe4e0a97
Valentin V. Vakrilov <vvakrilov@outlook.com>
2023-03-18 21:03:48 +0200

    Inheritance, Lab 01. Food solution

--- 03.Inheritance/01.Lab/01.Food/project/food.py
+++ 03.Inheritance/01.Lab/01.Food/project/food.py
@@ -2,3 +2,4 @@ class Food:
     def __init__(self, expiration_date: str, ):
         self.expiration_date = expiration_date
 
+# update
--- 03.Inheritance/01.Lab/01.Food/project/fruit.py
+++ 03.Inheritance/01.Lab/01.Food/project/fruit.py
@@ -5,3 +5,4 @@ class Fruit(Food):
     def __init__(self, name, expiration_date):
         super().__init__(expiration_date)
         self.name = name
+# update
